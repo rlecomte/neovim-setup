61e3620662605f34ab0785565f1584de1a0a5fbf
Romain Lecomte <romain@conduktor.io>
2024-07-15 15:11:14 +0200

    lsp mapping

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,3 +1,22 @@
+local telescope = require("telescope")
+telescope.setup {
+  defaults = {
+		path_display={"smart"},
+  },
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    },
+    oldfiles = {
+      theme = "dropdown",
+    },
+    live_grep = {
+      theme = "dropdown",
+    }
+  },
+  path_display= { "truncate" }
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-t>', builtin.find_files, {})
 vim.keymap.set('n', '<C-n>', builtin.oldfiles, {})
